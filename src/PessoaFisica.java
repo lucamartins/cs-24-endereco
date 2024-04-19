97697ace1402ae4823630a059aba447d449fe25f
Luca Santos Martins <lucamartins.dev@gmail.com>
2024-04-19 15:01:00 -0300

    feat: add PessoaFisica, which references the recently added Endereco

---
+++ src/PessoaFisica.java
@@ -0,0 +1,10 @@
+import endereco.Endereco;
+
+import java.util.Date;
+
+public class PessoaFisica {
+    private String nome;
+    private String sexo;
+    private Date dataNascimento;
+    private Endereco endereco;
+}
